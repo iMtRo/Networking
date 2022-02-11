1391d8effa1654014662e9a993cb5f119a5708fd
iMtRo <maxmudovilyos75@gmail.com>
2022-02-11 13:01:00 +0500

    Initial Commit

---
+++ Networking/NetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingApp.swift
+//  Networking
+//
+//  Created by user on 11/02/22.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
